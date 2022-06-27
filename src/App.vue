0825f0b2209391ea369258bd0df7712c3d1f5eae
zhangmengyang <2655419131@qq.com>
2022-06-27 19:33:19 +0800

    完善了底部导航的初步功能

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,40 @@
 <template>
-  <div></div>
+  <div>
+    <MyHeader title="tabbar"></MyHeader>
+    <MyFooter :tabList="tabList"></MyFooter>
+  </div>
 </template>
 
 <script>
-export default {}
+import MyHeader from './components/MyHeader.vue'
+import MyFooter from './components/MyFooter.vue'
+export default {
+  components: {
+    MyHeader,
+    MyFooter
+  },
+  data() {
+    return {
+      tabList: [
+        {
+          iconText: 'icon-shangpinliebiao',
+          text: '商品列表',
+          componentName: 'MyGoodsList'
+        },
+        {
+          iconText: 'icon-sousuo',
+          text: '商品搜索',
+          componentName: 'MyGoodsSearch'
+        },
+        {
+          iconText: 'icon-user',
+          text: '我的信息',
+          componentName: 'MyUserInfo'
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <style></style>
